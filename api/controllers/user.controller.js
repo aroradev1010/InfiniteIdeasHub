43f2335d3d4be685fb14398974d8601d1bbaff98
aroradev1010 <aroradev1010@gmail.com>
2024-01-31 15:50:33 +0530

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -52,3 +52,15 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id != req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete the user"));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User deleted successfully");
+  } catch (error) {
+    next(error);
+  }
+};
