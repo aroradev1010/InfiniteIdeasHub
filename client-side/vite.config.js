99bbb7be984a3bc3a6e3a63923062cfa6b5c9222
aroradev1010 <aroradev1010@gmail.com>
2024-02-05 18:27:30 +0530

    Revert "update"
    This reverts commit 6f5c44c9383abff98a8c2cee1dbecb5aeb788e93.

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://infinite-ideas-hub.vercel.app",
-        secure: true,
+        secure: false,
       },
     },
   },
