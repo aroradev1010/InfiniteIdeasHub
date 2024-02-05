8df1e21b74f5a58fba0391faa3d1593ca486d479
aroradev1010 <aroradev1010@gmail.com>
2024-02-05 18:36:22 +0530

    Revert "deployment"

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://infinite-ideas-hub.vercel.app",
+        target: "http://localhost:3000",
         secure: false,
       },
     },
