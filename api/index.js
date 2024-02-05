6f5c44c9383abff98a8c2cee1dbecb5aeb788e93
aroradev1010 <aroradev1010@gmail.com>
2024-02-05 18:12:17 +0530

    update

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://infinite-ideas-hub-frontend.vercel.app",
+    origin: "https://infinite-ideas-hub.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
